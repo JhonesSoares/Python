6d02117102e0ed0ffed4742d1d39a63e192b127d
Jhones Soares <jhones.soares.neves@gmail.com>
2024-08-22 22:26:47 -0300

    atualizacao

---
+++ Python Udemy/Intermediário/exe39_parametros_mutaveis.py
@@ -0,0 +1,25 @@
+
+# PROBLEMA DOS PARÂNMETROS MUTÁVEIS EM FUNÇOES NO PYTHON
+
+
+def adiciona_clientes(nome, lista=None):
+    if lista is None:
+        lista = []
+    lista.append(nome)
+    return lista
+
+
+cliente1 = adiciona_clientes('luiz')
+adiciona_clientes('Joana', cliente1)
+adiciona_clientes('Fernando', cliente1)
+cliente1.append('Edu')
+
+cliente2 = adiciona_clientes('Helena')
+adiciona_clientes('Maria', cliente2)
+
+cliente3 = adiciona_clientes('Moreira')
+adiciona_clientes('Vivi', cliente3)
+
+print(cliente1)
+print(cliente2)
+print(cliente3)
